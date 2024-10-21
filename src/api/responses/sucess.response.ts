4392d7dcbbd10c7bd180eb4f2f5d7c4089a69a7a
Kajlongero <dev.kajlongero@gmail.com>
2024-10-21 15:09:08 -0400

    Features:   - DB Design with Prisma v1.0 - Handling authentication

---
+++ src/api/responses/sucess.response.ts
@@ -0,0 +1,16 @@
+import { type Request, type Response } from "express";
+
+export const SuccessResponseBoth = <T>(
+  req: Request,
+  res: Response,
+  data: T,
+  message: string = "",
+  statusCode: number = 200
+) => {
+  res.status(statusCode).json({
+    error: false,
+    data,
+    message,
+    statusCode,
+  });
+};
